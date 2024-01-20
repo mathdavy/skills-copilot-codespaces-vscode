c1592fc9e978446d04a1f98d11f995bb2b6c13f8
Matheus Davy <49358862+mathdavy@users.noreply.github.com>
2024-01-20 20:27:28 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,82 @@
+// Create Web Server 
+// Create Database Connection
+// Create Schema
+// Create Model
+// Create Routes
+// Create Views
+
+// Import Modules
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Comment = require('./models/comment');
+
+// Create Express App
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/comment-system', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Create Routes
+app.get('/comments', async (req, res) => {
+  const comments = await Comment.find();
+  res.send(comments);
+});
+
+app.post('/comments', async (req, res) => {
+  const comment = new Comment({
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment
+  });
+
+  await comment.save();
+
+  res.send(comment);
+});
+
+// Start Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Create Views
+// Create views folder
+// Create index.html
+// Create comment.html
+// Create comments.html
+
+// Create index.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Comment System</title>
+  </head>
+  <body>
+    <h1>Comment System</h1>
+    <form action="/comments" method="POST">
+      <input type="text" name="name" placeholder="Name" /><br />
+      <input type="email" name="email" placeholder="Email" /><br />
+      <textarea name="comment" placeholder="Comment"></textarea><br />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
+
+// Create comment.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Comment System</title>
+  </head>
+  <body>
+    <h1>Comment System</h1>
+    <form action="/comments" method="POST">
+      <input type="text" name="name" placeholder="Name" /><br />
+      <input type="email" name="email" placeholder="Email" /><br
